44122f4d70542e8a51763fde39bd9a5e41ccd737
csslick <digitone@naver.com>
2019-01-28 22:31:02 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+  $(".main-visual").vegas({
+    slides: [
+        { src: "../images/main-visual1.jpg" },
+        { src: "../images/main-visual2.jpg" },
+        { src: "../images/main-visual3.jpg" },
+        { src: "../images/main-visual4.jpg" }
+    ],
+    timer: true,
+    // transition: 'slideLeft2',
+    delay: 5000
+  });  
+});
