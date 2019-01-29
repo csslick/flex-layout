d510145789c20e20735af37dc5f335b68ca8dbbd
csslick <digitone@naver.com>
2019-01-29 10:15:57 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 $(function(){
 
-  $(".main-visual").vegas({
-    slides: [
-        { src: "../images/main-visual1.jpg" },
-        { src: "../images/main-visual2.jpg" },
-        { src: "../images/main-visual3.jpg" },
-        { src: "../images/main-visual4.jpg" }
-    ],
-    timer: true,
-    // transition: 'slideLeft2',
-    delay: 5000
-  });  
+  $('#content').imagesLoaded({ }, function(){
+    console.log('All images loaded');
+
+    $(".main-visual").vegas({
+      slides: [
+          { src: "images/main-visual1.jpg" },
+          { src: "images/main-visual2.jpg" },
+          { src: "images/main-visual3.jpg" },
+          { src: "images/main-visual4.jpg" }
+      ],
+      timer: true,
+      // transition: 'slideLeft2',
+      delay: 5000
+    });  
+  });
+
+  // $('#content .main-visual')
+  //   .css({
+  //     backgroundImage: 'url("images/main-visual2.jpg")'
+  //   })
+
+
 });
